9d3fa75ecd55eba7bd5974366387e188cdaaff80
Karan Gupte <karangupte@Karans-MBP.lan>
2018-04-29 17:49:57 -0400

    New Roles

---
+++ provision_osx/provision_my_mac.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+working_dir=$(pwd)
+
+# Install pip
+sudo easy_install pip
+
+# Install VirtualEnv
+pip install Virtualenv
+
+# Create a virtual environment and activate it 
+mkdir -p ~/VirtualEnvs
+cd ~/VirtualEnvs
+if [ ! -e ~/VirtualEnvs/ansible_venv ]
+then  
+    virtualenv -p python3.5 ansible_venv
+fi
+. ansible_venv/bin/activate
+
+# Install ansible 
+pip install ansible --upgrade
+
+# Run the ansible playbook
+cd $working_dir
+ansible-playbook playbook.yml
